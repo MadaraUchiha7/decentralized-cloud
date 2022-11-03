96f5142ad79751f1cf4114b1f27aa638a9994189
Brahmanand Swami <brahmanandswami15@gmail.com>
2022-11-03 18:26:47 +0530

    Change Get Started button

--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -1,34 +1,31 @@
-import React from 'react'
-import "./homeScreen.css"
-import img3 from "../images/bubble.png"
+import React from 'react';
+import './homeScreen.css';
+import img3 from '../images/bubble.png';
 import Header from '../components/Header/Header';
 
 const Homescreen = () => {
   return (
-    
-      
-      <div className="hero">
-       <Header/>
-       <div className="content">
-           <h3>Welcome to the</h3>
-           <h1>Decentralized<br></br>cloud</h1>
-           <button type="button">Take a tour</button>
-       </div>
-     
-    
-      <div className="bubbles">
-         <img src={img3} alt=""/>
-         <img src={img3} alt=""/>
-         <img src={img3} alt=""/>
-         <img src={img3} alt=""/>
-         <img src={img3} alt=""/>
-         <img src={img3} alt=""/>
-         <img src={img3} alt=""/>
-        
-       </div> 
+    <div className="hero">
+      <Header />
+      <div className="content">
+        <h3>Welcome to the</h3>
+        <h1>
+          Decentralized<br></br>cloud
+        </h1>
+        <button type="button">Get Started</button>
       </div>
-    
-  );
-}
 
-export default Homescreen
+      <div className="bubbles">
+        <img src={img3} alt="" />
+        <img src={img3} alt="" />
+        <img src={img3} alt="" />
+        <img src={img3} alt="" />
+        <img src={img3} alt="" />
+        <img src={img3} alt="" />
+        <img src={img3} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Homescreen;
